a27e183ae5a3e559d5f76ec3a1b15735d85749b0
abhinavsrivastava <abhinavsrivastav687@gmail.com>
2023-05-12 11:53:11 +0530

    impelement withdraw money

--- src/HDFCBankAccount.java
+++ src/HDFCBankAccount.java
@@ -1,2 +1,25 @@
-public class HDFCBankAccount {
+public class HDFCBankAccount implements BankAccount {
+    private int balance;
+
+    public HDFCBankAccount(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance(int balance) {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+
+    @Override
+    public Boolean withdrawMoney(int money) {
+        if (this.balance >= money) {
+            this.balance = this.balance - money;
+            return true;
+        }
+        return false;
+    }
 }
